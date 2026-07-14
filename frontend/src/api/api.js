0802ba7b364d3d26c932cec350a6a9eca3cd7203
Sruthi <sruthigampa20@gmail.com>
2026-07-14 10:38:45 +0530

    fix: integrate frontend with final backend APIs

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "http://127.0.0.1:8000",
+
   headers: {
     "Content-Type": "application/json",
   },
