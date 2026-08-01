6c26e84554a385fd8a034390add9e71d7d0cabb7
jahnaviamballa-jpg <jahnaviamballa@gmail.com>
2026-08-01 06:28:47 +0530

    Fix employee API base URL

--- frontend/src/api/employeeApi.js
+++ frontend/src/api/employeeApi.js
@@ -1,31 +1,24 @@
-import axios from "axios";
+import api from "./api";
 import { getRestaurantId } from "../utils/restaurant";
-
-const API = "http://127.0.0.1:8000";
 
 export const getEmployees = async () => {
   const restaurantId = getRestaurantId();
 
-  const response = await axios.get(
-    `${API}/employees/restaurant/${restaurantId}`
+  const response = await api.get(
+    `/employees/restaurant/${restaurantId}`
   );
 
   return response.data;
 };
 
 export const getEmployee = async (id) => {
-  const response = await axios.get(
-    `${API}/employees/${id}`
-  );
-
+  const response = await api.get(`/employees/${id}`);
   return response.data;
 };
 
-export const createEmployee = async (
-  employee
-) => {
-  const response = await axios.post(
-    `${API}/employees/`,
+export const createEmployee = async (employee) => {
+  const response = await api.post(
+    "/employees/",
     employee
   );
 
@@ -36,19 +29,17 @@ export const updateEmployee = async (
   id,
   employee
 ) => {
-  const response = await axios.put(
-    `${API}/employees/${id}`,
+  const response = await api.put(
+    `/employees/${id}`,
     employee
   );
 
   return response.data;
 };
 
-export const deleteEmployee = async (
-  id
-) => {
-  const response = await axios.delete(
-    `${API}/employees/${id}`
+export const deleteEmployee = async (id) => {
+  const response = await api.delete(
+    `/employees/${id}`
   );
 
   return response.data;
